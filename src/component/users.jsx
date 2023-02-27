38e3c9d8abda64eccea8059ed53de75db749c8b0
Denis <generalpokrichkin@icloud.com>
2023-02-27 04:44:43 +0300

    add finished project

--- src/component/users.jsx
+++ src/component/users.jsx
@@ -2,19 +2,24 @@ import React, { useState } from "react";
 import api from "../api";
 
 const Users = () => {
-	// console.log(api.users.fetchAll());
 	const [users, setUsers] = useState(api.users.fetchAll());
 
 	const handelDelete = userId => {
-		setUsers(prevState => prevState);
+		setUsers(prevState => prevState.filter(item => item._id !== userId));
 	};
 
 	const renderPhrase = number => {
-		return users.length;
+		const numbers =
+			number > 4 || number === 1
+				? `${number} человек тусанет`
+				: `${number} человека тусанут`;
+
+		return <span className="badge bg-primary">{numbers} с тобой сегодня</span>;
 	};
 
-	return (
+	return users.length !== 0 ? (
 		<>
+			<h2>{renderPhrase(users.length)}</h2>
 			<table className="table">
 				<thead>
 					<tr>
@@ -30,24 +35,33 @@ const Users = () => {
 						<tr key={item._id}>
 							<td>{item.name}</td>
 							<td>{item.profession.name}</td>
-							<td>{item.qualities.map(item => item.name)}</td>
+							<td>
+								{item.qualities.map(item => (
+									<p key={item._id} className={`badge bg-${item.color} m-1`}>
+										{item.name}
+									</p>
+								))}
+							</td>
 							<td>{item.completedMeetings}</td>
-							<td>{`${item.rate}/5`}</td>
-							<td
-								className="badge bg-danger  m-2"
-								onClick={() => handelDelete(users)}
-							>
-								delete
+							<td>{`${item.rate} /5`}</td>
+							<td>
+								<button
+									key={item._id}
+									className="btn btn-danger btn-sm m-2"
+									onClick={() => handelDelete(item._id)}
+								>
+									delete
+								</button>
 							</td>
 						</tr>
 					))}
 				</tbody>
 			</table>
-			{/* <button id={users._id} className="btn btn-danger btn-sm m-2">
-				delete
-			</button> */}
 		</>
+	) : (
+		<h2>
+			<span className="badge bg-danger">Не кто не тусанет с тобой сегодня</span>
+		</h2>
 	);
 };
-
 export default Users;
